c24857f333e624790763b76fc9c2f90c26019b02
Tom R <99020119+Tom-Roche-kinoah@users.noreply.github.com>
2022-07-20 11:49:11 +0200

    feat: build standard menu structure (ul/li/a)

--- front/src/components/Menu/index.js
+++ front/src/components/Menu/index.js
@@ -3,11 +3,62 @@ import { NavLink } from 'react-router-dom';
 function Menu() {
   return (
     <nav className="menu">
-      <NavLink to="/tableau-de-bord">Tableau de bord</NavLink>
-      <NavLink to="/tournois">Tournois</NavLink>
-      <NavLink to="/club">Club</NavLink>
-      <NavLink to="/membres">Membres</NavLink>
-      <NavLink to="/classements">Classements</NavLink>
+      <ul>
+        <li>
+          <NavLink
+            to="/tableau-de-bord"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Tableau de bord
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/tournois"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Tournois
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/club"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Club
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/membres"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Membres
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/classements"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Classements
+          </NavLink>
+        </li>
+
+        <li>
+          <NavLink
+            to="/aide"
+            className={({ isActive }) => (isActive ? 'menu-link menu-link--selected' : 'menu-link')}
+          >
+            Aide
+          </NavLink>
+        </li>
+
+      </ul>
     </nav>
   );
 }
