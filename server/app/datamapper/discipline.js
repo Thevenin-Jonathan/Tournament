6a85b97fd4c2b520a2544734886595d99dc50d3b
tinopajaro <tinop@orange.fr>
2022-07-20 23:57:46 +0200

    config: Datamapper discipline - insertOne

--- server/app/datamapper/discipline.js
+++ server/app/datamapper/discipline.js
@@ -1,4 +1,3 @@
-// insertOne()
 // updateOne(id)
 // deleteOne(id)
 // Penser à JSDoc sur chaque fonction.
@@ -7,7 +6,7 @@ const pool = require("../config/database");
 
 // findAll() Recupere toutes les disciplines
 async function findAll() {
-    const result = await pool.query("SELECT * FROM discipline");
+    const result = await pool.query(`SELECT * FROM "discipline"`);
     const disciplines = result.rows;
     console.log(disciplines);
     pool.end();
@@ -16,14 +15,27 @@ async function findAll() {
 
 // findById(id) Recupere une discipline en function de son id
 async function findById(disciplineId) {
-    const result = await client.query(`SELECT * FROM discipline WHERE "id" = $1;`, [disciplineId]);
+    const result = await pool.query(`SELECT * FROM "discipline" WHERE "id" = $1;`, [disciplineId]);
     return result.rows[0];
+};
+
+// insertOne()
+async function insertOne(newDiscipline) {
+    const sql = {
+      text: `
+          INSERT INTO discipline ("name")
+          VALUES($1);`,
+      values: [newDiscipline]
+    };
+  
+    const result = await pool.query(sql);
+  
+    return result.rowCount;
   };
-
-
 
 module.exports = {
     findAll,
-    findById
+    findById,
+    insertOne
 }
 
