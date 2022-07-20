fc8c5b226a853ae29916a5894abcf4c6fbd7a377
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-20 17:25:14 +0200

    config: add v2 migration

--- server/sqitch/migrations/verify/v2.sql
+++ server/sqitch/migrations/verify/v2.sql
@@ -2,6 +2,82 @@
 
 BEGIN;
 
--- XXX Add verifications here.
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.table_constraints
+      WHERE  constraint_name  = 'user_email_check'
+        AND constraint_type = 'CHECK';
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Check user email constraint on user not found !';
+    END IF;
+END;
+$$;
+
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.table_constraints
+      WHERE  constraint_name  = 'club_email_check'
+        AND constraint_type = 'CHECK';
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Check club email constraint on user not found !';
+    END IF;
+END;
+$$;
+
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.table_constraints
+      WHERE  constraint_name  = 'club_phone_check'
+        AND constraint_type = 'CHECK';
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Check club phone constraint on user not found !';
+    END IF;
+END;
+$$;
+
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.table_constraints
+      WHERE  constraint_name  = 'user_phone_check'
+        AND constraint_type = 'CHECK';
+    IF NOT FOUND THEN
+        RAISE EXCEPTION 'Check user phone constraint on user not found !';
+    END IF;
+END;
+$$;
+
+SELECT "id", "label" FROM "result";
+SELECT "id", "name" FROM "gender";
+SELECT "phone" FROM "user";
+SELECT "gender_id" FROM "user";
+SELECT "result_id" FROM "match_has_team";
+
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.columns
+      WHERE  table_name  = 'user'
+        AND column_name = 'gender';
+    IF FOUND THEN
+        RAISE EXCEPTION 'Gender column does not found !';
+    END IF;
+END;
+$$;
+
+DO $$
+BEGIN
+    PERFORM TRUE
+       FROM   information_schema.columns
+      WHERE  table_name  = 'match_has_team'
+        AND column_name = 'score';
+    IF FOUND THEN
+        RAISE EXCEPTION 'Score column does not found !';
+    END IF;
+END;
+$$;
 
 ROLLBACK;
