d22053c9ef15b65f82444e55040693eba715cf58
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-21 18:04:12 +0200

    feat: add findByID func

--- server/app/datamapper/user.js
+++ server/app/datamapper/user.js
@@ -7,5 +7,11 @@ module.exports = {
     return result.rows;
   },
 
+  async findById(id) {
+    const result = pool.query(`SELECT * FROM "user" WHERE "id" = $1`, [id]);
+    pool.end();
+    return result.rows[0];
+  },
+
 
 }
