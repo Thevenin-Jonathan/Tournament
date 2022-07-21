b8bd3b957af8e4ed0cd0ed6d557b47543fb0ea75
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-21 18:04:39 +0200

    feat: add insertOne func

--- server/app/datamapper/user.js
+++ server/app/datamapper/user.js
@@ -13,5 +13,32 @@ module.exports = {
     return result.rows[0];
   },
 
+  async insertOne(user) {
+    const result = pool.query(
+    `
+      INSERT INTO "user"
+        ("firstname", "lastname", "address", "birthdate", "is_active", "email", "password", "url_avatar", "player_license", "club_id", "role_id", "phone", "gender_id")
+      VALUES
+        ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13);
+      RETURNING *
+    `,
+    [
+      user.firstname,
+      user.lastname,
+      user.address || null,
+      user.phone || null,
+      user.birthdate,
+      user.isActive,
+      user.email,
+      user.password,
+      user.urlAvatar || null,
+      user.playerLicense || null,
+      user.genderId,
+      user.clubId,
+      user.roleId
+    ])
+    pool.end();
+    return result.rows[0];
+  },
 
 }
