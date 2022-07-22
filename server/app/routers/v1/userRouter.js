cea0c07edcc756a10de050bd77969077ed63620d
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-22 18:14:28 +0200

    config: add wrapper controller to all user routes

--- server/app/routers/v1/userRouter.js
+++ server/app/routers/v1/userRouter.js
@@ -1,13 +1,14 @@
 const router = require("express").Router();
+const controllerWrapper = require("../../services/controllerWrapper");
 const controller = require("../../controllers/userController");
 
 router.route("/")
-  .get(controller.getAll)
-  .post(controller.create);
+  .get(controllerWrapper(controller.getAll))
+  .post(controllerWrapper(controller.create));
 
 router.route("/:id")
-  .get(controller.getOne)
-  .patch(controller.update)
-  .delete(controller.destroy);
+  .get(controllerWrapper(controller.getOne))
+  .patch(controllerWrapper(controller.update))
+  .delete(controllerWrapper(controller.destroy));
 
 module.exports = router;
