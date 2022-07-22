8319221941dcb18a6f85299537f61118266ec26b
Houss <elhandouzelhouceine@gmail.com>
2022-07-22 10:04:08 +0200

    upd: type datamapper

--- server/app/datamapper/type.js
+++ server/app/datamapper/type.js
@@ -1,18 +1,15 @@
 const pool = require("../config/database");
 
 
-
-module.exports = {
-
     /**
      * @returns Tous les types dans la base de donnée
      */
 
-    async findAll() {
+    async function findAll() {
         const result = await pool.query(`SELECT * FROM "type"`);
         pool.end();
         return result.rows;
-    },
+    };
 
     /**
      * Récupère par sont id
@@ -21,14 +18,14 @@ module.exports = {
      */
 
     // findById(typeId) Recupere un type en function de son id
-    async findByPk(typeId) {
+    async function findByPk(typeId) {
         const result = await pool.query(`SELECT * FROM "type" WHERE "id" = $1;`, [typeId]);
         if (result.rowCount === 0) {
             return undefined;
         }
         pool.end();
         return result.rows[0];
-    },
+    };
 
     /**
      * Ajoute dans la base de données
@@ -36,7 +33,7 @@ module.exports = {
      * @returns Le type insérer
      */
     // insertOne() Ajoute un nouveau type à la BDD
-    async insertOne(type) {
+    async function insertOne(type) {
         const newType = await pool.query(
             `
         INSERT INTO type ("name")
@@ -47,10 +44,10 @@ module.exports = {
         );
         pool.end();
         return newType.rows[0];
-    },
+    };
 
     // updateOne(id) Change les informations (name) d'un type
-    async updateOne(typeId, typeName) {
+    async function updateOne(typeId, typeName) {
         const updateType = {
             text: `
               UPDATE "type" SET 
@@ -63,13 +60,20 @@ module.exports = {
         const result = await pool.query(sql);
         pool.end();
         return result.rows[0];
-    },
+    };
 
     // deleteOne(id) Enleve une discipline de la BDD
-    async deleteOne(disciplineId) {
+    async function deleteOne(disciplineId) {
         const result = await pool.query(`DELETE FROM "discipline" WHERE "id" = $1;`, [disciplineId]);
         pool.end();
         return result.rowCount;
-    },
+    };
 
+    module.exports = {
+        findAll,
+        findById,
+        insertOne,
+        updateOne,
+        deleteOne
 };
+
