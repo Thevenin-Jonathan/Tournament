1bad28d47b585049ed7c0001add55c076ba7e6c5
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-22 11:30:43 +0200

    config: add user controller structure file

---
+++ server/app/controllers/userController.js
@@ -0,0 +1,32 @@
+const bcrypt = require("bcrypt");
+const userDatamapper = require("../datamapper/user");
+
+async function register(req, res) {
+  
+};
+
+async function getAll(req, res) {
+};
+
+async function getOne(req, res) {
+};
+
+async function create(req, res) {
+};
+
+async function update(req, res) {
+
+}
+
+async function destroy(req, res) {
+
+};
+
+module.exports = {
+  register,
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy
+}
