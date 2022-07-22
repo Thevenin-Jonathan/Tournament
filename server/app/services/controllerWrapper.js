bfed0c5381bacfd0d7190f93c58cc3e4deb64af5
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-22 18:41:46 +0200

    upd: add JSDoc

--- server/app/services/controllerWrapper.js
+++ server/app/services/controllerWrapper.js
@@ -1,7 +1,12 @@
-module.exports = function wrapper(controller) {
+/**
+ * Controller wrapper with try catch for manage errors
+ * @param {function} controllerFunc controller function to try catch
+ * @returns {function} controller function as middleware function
+ */
+module.exports = function wrapper(controllerFunc) {
     return async function tryCatch(req, res, next) {
     try {
-      await controller(req, res, next);
+      await controllerFunc(req, res, next);
     } catch (error) {
       next(error);
     }
