dbceb5ee1fc796c35611c2c8984aff940b5799e8
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-22 13:41:10 +0200

    fix: remove some useless things and fix message

--- server/app/controllers/userController.js
+++ server/app/controllers/userController.js
@@ -1,10 +1,4 @@
-const debug = require('debug')('app:server');
-const bcrypt = require("bcrypt");
 const userDatamapper = require("../datamapper/user");
-
-async function register(req, res) {
-  
-};
 
 async function getAll(req, res) {
   const users = await userDatamapper.findAll();
@@ -12,7 +6,7 @@ async function getAll(req, res) {
 };
 
 async function getOne(req, res) {
-  const id = Number(req.params.id);
+  const id = req.params.id;
   const user = await userDatamapper.findById(id);
   res.json(user);
 };
@@ -22,8 +16,8 @@ async function create(req, res) {
   const user = await userDatamapper.exist(userData);
 
   if (user) {
-    // throw new Error("User already exist in DB");
-    res.json({message: "User already exist in DB"})
+    // throw new Error("User is already exist in DB");
+    return res.json({message: "User is already exist in DB"})
   }
 
   const newUser = await userDatamapper.insertOne(userData);
