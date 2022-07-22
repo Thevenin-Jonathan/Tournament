9efcb922d0569f2537a0b963a0e9a2a22c8bb8d3
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-22 11:26:19 +0200

    config: add routes

--- server/app/routers/v1/userRouter.js
+++ server/app/routers/v1/userRouter.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const controller = require("../../controllers/userController");
 
+router.route("/")
+  .get(controller.getAll)
+  .post(controller.create);
+
+router.route("/:id")
+  .get(controller.getOne)
 
 module.exports = router;
