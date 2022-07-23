035f75794819164b030e97a620a8252d02abeccb
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-23 21:13:53 +0200

    config: add front files

--- app/config/database.js
+++ app/config/database.js
@@ -1,5 +1,6 @@
 require("dotenv").config( {path: `${__dirname}/../../.env` });
 const connectionString = process.env.DATABASE_URL;
+// const connectionString = process.env.PGURL;
 
 const { Pool } = require('pg');
 // pools will use environment variables
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const path = require("path");
 const express = require("express");
 // const debug = require('debug')('app');
 const app = express();
@@ -23,8 +24,16 @@ app.use(express.urlencoded({ extended: true }));
 //   }
 // });
 
+app.use(express.static(path.join(__dirname, 'public')));
+
+
 /** Router **/
 app.use("/", router);
+
+
+app.get('/*', (_, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
 
 const port = process.env.PORT ?? 3001;
 app.listen(port, () => {
