ade7a7d56c71fb0181ec3b5d34a3aa4591fc6545
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-23 16:28:57 +0200

    feat: add findByMatchIdAndTeamId func

--- server/app/datamappers/matchHasTeam.js
+++ server/app/datamappers/matchHasTeam.js
@@ -10,6 +10,23 @@ async function findAll() {
   return result.rows;
 }
 
+/**
+ * Return all relations and informations between one match and one team from database
+ * @param {number} matchId match identifiant
+ * @param {number} teamId team identifiant
+ * @returns {object} match and his team data
+ */
+async function findByMatchIdAndTeamId(matchId, teamId) {
+  const result = await pool.query(
+    `
+      SELECT * FROM "match_has_team"
+      WHERE "match_id" = $1
+        AND team_id = $2;
+    `,
+    [matchId, teamId]);
+  return result.rows[0];
+}
+
 
 module.exports = {
   findAll,
