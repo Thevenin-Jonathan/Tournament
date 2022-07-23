982d4b64aaf85b74cfde4cdae6ae98a31877284f
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-23 20:46:29 +0200

    config: add SSL on PG

--- app/config/database.js
+++ app/config/database.js
@@ -1,5 +1,5 @@
 require("dotenv").config( {path: `${__dirname}/../../.env` });
-const connectionString = process.env.DATABASE_URL;
+const connectionString = process.env.DATABASE_URL + "?ssl=true";
 
 const { Pool } = require('pg');
 // pools will use environment variables
