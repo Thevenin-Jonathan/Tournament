ebfb0baef6a243a2d1129b3b5171f3aa7c5428b3
Tom R <99020119+Tom-Roche-kinoah@users.noreply.github.com>
2022-07-23 19:25:45 +0200

    feat: 404 page app (connected)

--- front/src/components/Error/index.js
+++ front/src/components/Error/index.js
@@ -5,6 +5,7 @@ function Error() {
     <main className="app-error content">
       <h1 className="title">Erreur 404 🙀</h1>
       <p>Manifestement vous etes perdu...</p>
+
       <img src={cat404} alt="Chapristi!" className="cat-404" />
     </main>
   );
