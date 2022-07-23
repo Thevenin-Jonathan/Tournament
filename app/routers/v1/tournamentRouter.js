dd2e2b4adbf02a0efe4d0ad02a164725299535e4
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-23 23:13:55 +0200

    config: reset main

--- app/routers/v1/tournamentRouter.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-
-router.get("/", async (req, res) => {
-  res.send("Hello Tournament")
-})
-
-module.exports = router;
--- front/src/components/Error/index.js
+++
@@ -1,5 +0,0 @@
-function Error() {
-  return <main className="error content">Erreur 404</main>;
-}
-
-export default Error;
