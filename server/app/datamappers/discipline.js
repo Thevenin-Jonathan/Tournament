ee4d8d270acf1cac6292707d1b72a4cb0b51f662
Houss <elhandouzelhouceine@gmail.com>
2022-07-23 20:53:04 +0200

    18-datamapper type OK TEST OK

--- server/app/datamappers/discipline.js
+++ server/app/datamappers/discipline.js
@@ -69,6 +69,9 @@ async function updateOne(name, id) {
     pool.end();
     return result.rows[0];
   };
+  (async function test() {
+    console.log(await updateOne("poul"));
+  })();
 
   module.exports = {
     findAll,
