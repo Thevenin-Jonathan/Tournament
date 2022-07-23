b0cf9b8b426548f36857faf7e8cfa0247c02a4eb
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-23 21:18:09 +0200

    config: add CORS

--- index.js
+++ index.js
@@ -9,20 +9,20 @@ const router = require("./app/routers");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// /** Cors **/
-// app.use((req, res, next) => {
-//   res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-//   res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+/** Cors **/
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://cassini-tournament.herokuapp.com');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-//   // response to preflight request
-//   if (req.method === 'OPTIONS') {
-//     res.sendStatus(200);
-//   }
-//   else {
-//     next();
-//   }
-// });
+  // response to preflight request
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  }
+  else {
+    next();
+  }
+});
 
 app.use(express.static(path.join(__dirname, 'public')));
 
