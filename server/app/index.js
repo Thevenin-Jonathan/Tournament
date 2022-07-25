c47b92cd300e924d58eb1018953c1c471bf916ea
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-25 20:26:13 +0200

    fix: add path require

--- server/app/index.js
+++ server/app/index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const debug = require('debug')('app');
+const path = require("path");
 const app = express();
 const router = require("./routers");
 const helmet = require("helmet");
