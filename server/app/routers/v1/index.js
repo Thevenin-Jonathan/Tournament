f1372687773b75fcdabe6a9b3f563beb043cdb91
tinopajaro <tinop@orange.fr>
2022-07-25 17:50:51 +0200

    config: clubController fait et testé + clubRouter

--- server/app/routers/v1/index.js
+++ server/app/routers/v1/index.js
@@ -4,10 +4,12 @@ const tournamentRouter = require("./tournamentRouter");
 const matchRouter = require("./matchRouter");
 const teamRouter = require("./teamRouter");
 const userRouter = require("./userRouter");
+const clubRouter = require("./clubRouter");
 
 router.use("/tournaments", tournamentRouter);
 router.use("/matchs", matchRouter);
 router.use("/teams", teamRouter);
 router.use("/users", userRouter);
+router.use("/clubs", clubRouter);
 
 module.exports = router;
