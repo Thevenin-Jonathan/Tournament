cce9aa340614b94c1b20183e9bbda16bd34cd95b
tinopajaro <tinop@orange.fr>
2022-07-25 16:53:29 +0200

    config: teamRouter fait

--- server/app/routers/v1/teamRouter.js
+++ server/app/routers/v1/teamRouter.js
@@ -1,5 +1,15 @@
 const router = require("express").Router();
+const controllerWrapper = require("../../services/controllerWrapper");
+const controller = require("../../controllers/teamController");
 
+router.route("/")
+  .get(controllerWrapper(controller.getAll))
+  .post(controllerWrapper(controller.create));
 
+router.route("/:id")
+  .get(controllerWrapper(controller.getOne))
+  .patch(controllerWrapper(controller.update))
+  .delete(controllerWrapper(controller.destroy));
 
 module.exports = router;
+
