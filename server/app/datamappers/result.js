31ef22ddaf8014ad7bff62dc1cc43d860e902d84
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-25 10:54:07 +0200

    feat: add result datamapper

---
+++ server/app/datamappers/result.js
@@ -0,0 +1,71 @@
+const pool = require("../config/database");
+
+/**
+ * Return all "result" from database
+ * @returns {result[]} result
+ */
+ async function findAll() {
+    const result = await pool.query(`SELECT * FROM "result"`);
+    return result.rows;
+};
+
+/**
+ * Return one "result" from database
+ * @param {number} id result identifiant
+ * @returns {object} result
+ */
+ async function findById(id) {
+    const result = await pool.query(`SELECT * FROM "result" WHERE "id" = $1`, [id]);
+    return result.rows[0];
+};
+
+/**
+ * Insert one "result" in database
+ * @param {string} label of result
+ * @returns {object} Return new result
+ */
+ async function insertOne(label) {
+    const result = await pool.query(
+        `INSERT INTO result("label")
+        VALUES ($1)
+        RETURNING *`, [label]
+    );
+    return result.rows[0];
+};
+
+/** 
+ * Delete one result from DB
+ * @param {number}  id of the result
+ * @returns {boolean} - true if the result is deleted
+*/
+ async function deleteOne(id) {
+    const result = await pool.query(
+        `DELETE FROM "result" WHERE "id" = $1`, [id]
+    );
+    return !!result.rowCount;
+};
+
+/** 
+ * Update result
+ * @param {string}  label of the result
+ * @param {number}  id of the result 
+ * @returns {Object} - "result" updated
+*/
+ async function updateOne(id, label) {
+    const result = await pool.query(
+        `UPDATE "result"
+        SET "label" = $1
+        WHERE "id" = $2
+        RETURNING *
+        `, [id, label]
+    );
+    return result.rows[0];
+};
+  
+module.exports = {
+    findAll,
+    findById,
+    insertOne,
+    deleteOne,
+    updateOne
+  }
