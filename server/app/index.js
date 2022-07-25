7159505a28280fcb409d5862f227dc347c3bb514
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-25 19:18:31 +0200

    config: add condition with NODE_ENV

--- server/app/index.js
+++ server/app/index.js
@@ -12,23 +12,36 @@ app.use(helmet());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-/** Cors **/
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+if (process.env.NODE_ENV === "prod") {
+  /** Cors **/
+  app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
 
-  // response to preflight request
-  if (req.method === 'OPTIONS') {
-    res.sendStatus(200);
-  }
-  else {
-    next();
-  }
-});
+    // response to preflight request
+    if (req.method === 'OPTIONS') {
+      res.sendStatus(200);
+    }
+    else {
+      next();
+    }
+  });
+
+  /** Static files **/
+  app.use(express.static(path.join(__dirname, 'public')));
+}
 
 /** Router **/
 app.use("/", router);
+
+/** Route front**/
+
+if (process.env.NODE_ENV === "prod") {
+  app.get('/*', (_, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  });
+}
 
 const port = process.env.PORT ?? 3001;
 app.listen(port, () => {
