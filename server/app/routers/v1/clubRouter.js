f3e4ead4b733cc704de26ca1c707f1501a5e0c0a
tinopajaro <tinop@orange.fr>
2022-07-26 23:41:42 +0200

    config: ajout de joi sur clubRouter

--- server/app/routers/v1/clubRouter.js
+++ server/app/routers/v1/clubRouter.js
@@ -8,11 +8,15 @@ const updateSchema = require("../../schemas/clubUpdate");
 router.route("/")
   .get(controllerWrapper(controller.getAll))
   .post(
+    validationWrapper(createSchema),
     controllerWrapper(controller.create));
+
 
 router.route("/:id")
   .get(controllerWrapper(controller.getOne))
-  .patch(controllerWrapper(controller.update))
+  .patch(
+    validationWrapper(updateSchema),
+    controllerWrapper(controller.update))
   .delete(controllerWrapper(controller.destroy));
 
 module.exports = router;
