8c915279da6549b9dc5ad5a88d58ccd603135894
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-26 23:48:08 +0200

    upd: improve code and fix status code

--- server/app/controllers/matchController.js
+++ server/app/controllers/matchController.js
@@ -1,4 +1,5 @@
 const matchDatamapper = require("../datamappers/match");
+const { ApiError, Api404Error } = require("../services/errorHandler");
 
 /**
  * Get all matchs from DB
@@ -8,12 +9,12 @@ const matchDatamapper = require("../datamappers/match");
  * @param {object} res express response object
  * @returns {json} JSON response with all matchs
  */
- async function getAll(_, res) {
-    const matchs = await matchDatamapper.findAll();
-    return res.json(matchs);
-  };
+async function getAll(_, res) {
+  const matchs = await matchDatamapper.findAll();
+  return res.json(matchs);
+};
 
-  /**
+/**
  * Get one match from DB
  * 
  * ExpressMiddleware signature
@@ -21,13 +22,13 @@ const matchDatamapper = require("../datamappers/match");
  * @param {object} res express response object
  * @returns {json} JSON response with one match
  */
-   async function getOne(req, res) {
-    const id = req.params.id;
-    const match = await matchDatamapper.findById(id);
-    return res.json(match);
-  };
+async function getOne(req, res) {
+  const id = req.params.id;
+  const match = await matchDatamapper.findById(id);
+  return res.json(match);
+};
 
-  /**
+/**
  * Add one match into DB
  * 
  * ExpressMiddleware signature
@@ -36,12 +37,12 @@ const matchDatamapper = require("../datamappers/match");
  * @returns {json} JSON response with the created match
  */
 async function create(req, res) {
-    const match = req.body;
-    const newMatch = await matchDatamapper.insertOne(match);
-    return res.json(newMatch);
-  };
+  const match = req.body;
+  const newMatch = await matchDatamapper.insertOne(match);
+  return res.status(201).json(newMatch);
+};
 
-  /**
+/**
  * Update one match into DB
  * 
  * ExpressMiddleware signature
@@ -50,18 +51,19 @@ async function create(req, res) {
  * @returns {json} JSON response with the updated match
  */
 async function update(req, res) {
-    const id = req.params.id;
-    const newMatch = req.body;
-    const match = await matchDatamapper.findById(id);
-  
-    if (!match) {
-      return res.json({message: "Match does not exist in DB"})
-    }
-    const updMatch = await matchDatamapper.updateOne(id, newMatch)
-    return res.json(updMatch)
-  }
+  const id = req.params.id;
+  const newMatch = req.body;
+  const match = await matchDatamapper.findById(id);
 
-  /**
+  if (!match) {
+    throw new Api404Error("Match does not exist in DB");
+  }
+  
+  const updMatch = await matchDatamapper.updateOne(id, newMatch)
+  return res.json(updMatch)
+}
+
+/**
  * Delete one match from DB
  * 
  * ExpressMiddleware signature
@@ -70,20 +72,20 @@ async function update(req, res) {
  * @returns {json} JSON response with one match
  */
 async function destroy(req, res) {
-    const id = parseInt(req.params.id);
-    const match = await matchDatamapper.findById(id);
-    
-    if (!match) {
-      return res.json({message: "Match does not exist in DB"})
-    }
-    await matchDatamapper.deleteOne(id);
-    return res.status(204).json();
-  };
-
-  module.exports = {
-    getAll,
-    getOne,
-    create,
-    update,
-    destroy
+  const id = parseInt(req.params.id);
+  const match = await matchDatamapper.findById(id);
+  
+  if (!match) {
+    return res.json({message: "Match does not exist in DB"})
   }
+  await matchDatamapper.deleteOne(id);
+  return res.status(204).json();
+};
+
+module.exports = {
+  getAll,
+  getOne,
+  create,
+  update,
+  destroy
+}
