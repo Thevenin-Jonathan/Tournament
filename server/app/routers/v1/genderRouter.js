ef87f91f4b2bc67866dd1e5fee7659fd35251fdb
tinopajaro <tinop@orange.fr>
2022-07-26 14:15:36 +0200

    config: ajout de joi dans le router gender

--- server/app/routers/v1/genderRouter.js
+++ server/app/routers/v1/genderRouter.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const controllerWrapper = require("../../services/controllerWrapper");
 const controller = require("../../controllers/genderController");
+const validationWrapper = require("../../services/validationWrapper");
+const createSchema = require("../../schemas/genderCreate");
+const updateSchema = require("../../schemas/genderUpdate");
 
 router.route("/")
   .get(controllerWrapper(controller.getAll))
-  .post(controllerWrapper(controller.create));
+  .post(
+    validationWrapper(createSchema),
+    controllerWrapper(controller.create));
 
 router.route("/:id")
   .get(controllerWrapper(controller.getOne))
-  .patch(controllerWrapper(controller.update))
+  .patch(
+    validationWrapper(updateSchema),
+    controllerWrapper(controller.update))
   .delete(controllerWrapper(controller.destroy));
 
 module.exports = router;
