47fee816f300f28526ef2d4a82d324f2832ef341
Houss <elhandouzelhouceine@gmail.com>
2022-07-26 14:42:12 +0200

    upd: roleCreate + roleUpdate +roleRouter upd with schema

--- server/app/routers/v1/roleRouter.js
+++ server/app/routers/v1/roleRouter.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const controllerWrapper = require("../../services/controllerWrapper");
 const controller = require("../../controllers/roleController");
+const validationWrapper = require("../../services/validationWrapper");
+const createSchema = require("../../schemas/roleCreate");
+const updateSchema = require("../../schemas/roleUpdate");
 
 router.route("/")
   .get(controllerWrapper(controller.getAll))
-  .post(controllerWrapper(controller.create));
+  .post(
+    validationWrapper(createSchema),
+    controllerWrapper(controller.create));
 
 router.route("/:id")
   .get(controllerWrapper(controller.getOne))
-  .put(controllerWrapper(controller.update))
+  .put(
+    validationWrapper(updateSchema),
+    controllerWrapper(controller.update))
   .delete(controllerWrapper(controller.destroy));
 
 module.exports = router;
