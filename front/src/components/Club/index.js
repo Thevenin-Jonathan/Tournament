469ab5c35f993fdf84c18a745899c8d7e8d36bb4
Cédric Bernard <bernardcedric@live.fr>
2022-07-27 17:11:20 +0200

    refactor : Club component (improve design)

--- front/src/components/Club/index.js
+++ front/src/components/Club/index.js
@@ -3,74 +3,32 @@ import clublogo from 'src/assets/logo-bayard-bad-blanc.png';
 function Club() {
   return (
     <main className="content club">
+      <h1 className="title">Profil du club</h1>
 
-      <div className="title">
-        <h1>Profil du club</h1>
-        <span className="members-nb">92 membres</span>
-      </div>
-
-      <div className="club-profil">
-        <div className="club-profil-infos">
-          <h2 className="club-name">Bayard Bad</h2>
-          <ul className="club-infos">
-            <li className="club-info">
-              <button type="button" className="list-item-btn">
-                <i className="fa fa-pencil" aria-hidden="true" />
-              </button>
-              Adresse: 175 avenue de Paris 35000 Rennes
-            </li>
-            <li className="club-info">
-              <button type="button" className="list-item-btn">
-                <i className="fa fa-pencil" aria-hidden="true" />
-              </button>
-              Email: bayardclub@free.fr
-            </li>
-            <li className="club-info">
-              <button type="button" className="list-item-btn">
-                <i className="fa fa-pencil" aria-hidden="true" />
-              </button>
-              Tél: 01 23 45 67 89
-            </li>
-            <li className="club-info">
-              <button type="button" className="list-item-btn">
-                <i className="fa fa-pencil" aria-hidden="true" />
-              </button>
-              Site web: www.bayardclub.com
-            </li>
-          </ul>
-        </div>
-        <div className="club-profil-image">
-          <img src={clublogo} className="club-logo" alt="Logo du club" />
-        </div>
-      </div>
-      <div className="club-description">
-        <button type="button" className="list-item-btn">
-          <i className="fa fa-pencil" aria-hidden="true" />
-        </button>
-        Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-        Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
-        ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa,
-        varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
-        molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
-        Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.
-        Pellentesque congue. Ut in risus volutpat libero pharetra tempor.
-        Cras vestibulum bibendum augue. Praesent egestas leo in pede.
+      <div className="club-card">
+        <h2 className="club-name">Bayard Bad</h2>
+        <img className="club-avatar" src={clublogo} alt="Logo du club" />
+        <ul>
+          <li>Nombre de membres :<span>92</span></li>
+          <li>Adresse :<span>175 avenue de Paris 35000 Rennes</span></li>
+          <li>Contact :<span>bayardclub@free.fr</span><span>01 23 45 67 89</span></li>
+          <li>Site internet : <span>www.bayardclub.com</span></li>
+          <li>Description :
+            <span>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+              Sed non risus. Suspendisse lectus tortor, dignissim sit amet, adipiscing nec,
+              ultricies sed, dolor. Cras elementum ultrices diam. Maecenas ligula massa,
+              varius a, semper congue, euismod non, mi. Proin porttitor, orci nec nonummy
+              molestie, enim est eleifend mi, non fermentum diam nisl sit amet erat.
+              Duis semper. Duis arcu massa, scelerisque vitae, consequat in, pretium a, enim.
+              Pellentesque congue. Ut in risus volutpat libero pharetra tempor.
+              Cras vestibulum bibendum augue. Praesent egestas leo in pede.
+            </span>
+          </li>
+        </ul>
       </div>
     </main>
   );
 }
 
 export default Club;
-
-// eslint-disable-next-line no-lone-blocks
-{ /* quand je clique ici, j'active ou désactive le mode d'édition
-
-  <button
-    className="list-item__iconbtn"
-    type="button"
-    onClick={() => onEditToggle(task.id)}
-  >
-    ✍️
-  </button>;
-
-*/ }
