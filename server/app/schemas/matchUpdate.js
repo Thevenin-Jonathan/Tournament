acd416bc939578e838c6991c47a725a143e56b69
Houss <elhandouzelhouceine@gmail.com>
2022-07-27 09:38:45 +0200

    upd: correction sans min()

--- server/app/schemas/matchUpdate.js
+++ server/app/schemas/matchUpdate.js
@@ -1,7 +1,7 @@
 const joi = require("joi");
 
 module.exports = joi.object({
-  note: joi.string().min(0),
+  note: joi.string(),
   state_id: joi.string().min(1),
   tournament_id: joi.string().min(1)
 });
