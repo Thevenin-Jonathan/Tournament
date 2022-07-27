1a1848d82808ac5ac810447d4126e7037e06c485
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-27 18:45:16 +0200

    upd: add condition with node_env for url api and server to front config

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -1,7 +1,17 @@
+let url, urlApi;
+
+if (process.env.NODE_ENV === "production") {
+  url = "https://cassini-tournament.herokuapp.com/";
+  urlApi = "https://cassini-tournament.herokuapp.com/api/v1";
+} else {
+  url = "http://localhost:3001";
+  urlApi = "http://localhost:3001/api/v1";
+}
+
 const config = {
-    serverUrl: 'http://localhost:3001',
+    serverUrl: url,
     api: {
-        baseUrl: 'http://localhost:3001/api/v1',
+        baseUrl: urlApi,
         accessToken: null,
     },
     path: {
