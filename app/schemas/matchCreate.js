0d7c0cac8d54376f010ccef12a4b0855bf22c192
Thevenin-Jonathan <warzonefury@hotmail.fr>
2022-07-27 18:38:27 +0200

    erase all test file

--- app/schemas/matchCreate.js
+++
@@ -1,7 +0,0 @@
-const joi = require("joi");
-
-module.exports = joi.object({
-  note: joi.string(),
-  state_id: joi.string().min(1).required(),
-  tournament_id: joi.string().min(1).required()
-});
