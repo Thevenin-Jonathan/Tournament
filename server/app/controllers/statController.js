c723b64feb720641befb04de933d596d19b646cb
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-07-28 17:17:26 +0200

    feat: add stat controller

---
+++ server/app/controllers/statController.js
@@ -0,0 +1,34 @@
+const debug = require("debug")("ct-stat");
+const statDatamapper = require("../datamappers/stat");
+
+/**
+ * Get all player stats from DB
+ * 
+ * ExpressMiddleware signature
+ * @param {object} _ express request object (not used)
+ * @param {object} res express response object
+ * @returns {json} JSON response with all player stats
+ */
+async function getAll(_, res) {
+  const stats = await statDatamapper.findAll();
+  return res.json(stats);
+};
+
+/**
+ * Get one user from DB
+ * 
+ * ExpressMiddleware signature
+ * @param {object} req express request object
+ * @param {object} res express response object
+ * @returns {json} JSON response with one user
+ */
+async function getOne(req, res) {
+  const id = req.params.id;
+  const stats = await statDatamapper.findById(id);
+  return res.json(stats);
+};
+
+module.exports = {
+  getAll,
+  getOne
+}
