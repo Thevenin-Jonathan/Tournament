c60c52a979d8705b657845baae0feeb22689bc56
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-07-30 15:40:18 +0200

    upd: match create schema

--- server/app/schemas/matchCreate.js
+++ server/app/schemas/matchCreate.js
@@ -2,9 +2,7 @@ const joi = require("joi");
 
 module.exports = joi.object({
   tournament_id: joi.number().min(1).required(),
-  team: joi.array().items(
-    joi.object({
-      id: joi.number().required()
-    })
-  ).min(1).max(2).required()
+  teams: joi.array().items(
+      joi.number().required()
+  ).length(2).required()
 });
