8f771f0da7a6ae60f4d03e7217e5205194b79b46
Tom R <99020119+Tom-Roche-kinoah@users.noreply.github.com>
2022-08-01 00:34:26 +0200

    feat: filter system on tournament view

--- front/src/components/Tournaments/index.js
+++ front/src/components/Tournaments/index.js
@@ -8,7 +8,8 @@ import TournamentCard from '../TournamentCard';
 function Tournaments() {
   const dispatch = useDispatch();
   const isAdmin = useSelector((state) => (state.user.isAdmin));
-  const tournaments = useSelector((state) => (state.tournament.tournaments));
+  const tournaments = useSelector((state) => (state.tournament));
+  const tournamentsList = tournaments.tournaments;
 
   useEffect(() => {
     dispatch({
@@ -16,9 +17,64 @@ function Tournaments() {
     });
   }, []);
 
-  const tournamentsFilter = (tournamentState) => tournaments.filter((tournament) => (
-    tournament.state_id === tournamentState
-  ));
+  const changeField = (value, input) => {
+    dispatch({
+      type: 'CHANGE_FIELD_TOURNAMENT',
+      input,
+      value,
+    });
+  };
+
+  // Pré filtrer la liste complete (filtres de la page Tournaments)
+  const buildFilterFromState = () => {
+    // objet vide dans lequel on push sur les clés, les champs cochés
+    const filter = {
+      state_id: [],
+      discipline_id: [],
+    };
+    // if (
+    //   !tournaments.filterState1
+    //   && !tournaments.filterState2
+    //   && !tournaments.filterState3
+    //   && !tournaments.filterState4) {
+    //   filter.state_id.push(1, 2, 3, 4);
+    // }
+    if (tournaments.filterState0) filter.state_id.push(1, 2, 3, 4);
+    if (tournaments.filterState1) filter.state_id.push(1);
+    if (tournaments.filterState2) filter.state_id.push(2);
+    if (tournaments.filterState3) filter.state_id.push(3);
+    if (tournaments.filterState4) filter.state_id.push(4);
+    if (tournaments.filterDiscipline0) filter.discipline_id.push(1, 2, 3, 4, 5);
+    if (tournaments.filterDiscipline1) filter.discipline_id.push(1);
+    if (tournaments.filterDiscipline2) filter.discipline_id.push(2);
+    if (tournaments.filterDiscipline3) filter.discipline_id.push(3);
+    if (tournaments.filterDiscipline4) filter.discipline_id.push(4);
+    if (tournaments.filterDiscipline5) filter.discipline_id.push(5);
+    return filter;
+  };
+
+  const filter = buildFilterFromState();
+
+  // à l'aide de l'objet "filter" construit à partir du state,
+  // on préfiltre le tableau des tournois
+  const filterData = (data, query) => {
+    const filteredData = data.filter((item) => {
+      // eslint-disable-next-line no-restricted-syntax
+      for (const key in query) {
+        if (item[key] === undefined || !query[key].includes(item[key])) {
+          return false;
+        }
+      }
+      return true;
+    });
+    return filteredData;
+  };
+
+  // filtre par status pour injecter dans les sections
+  const tournamentsFilter = (tournamentState) => filterData(tournamentsList, filter)
+    .filter((tournament) => (
+      tournament.state_id === tournamentState
+    ));
 
   return (
     <main className="tournaments content">
@@ -78,29 +134,78 @@ function Tournaments() {
 
       <div className="tournaments-filter">
         <h2><i className="fa fa-sliders" aria-hidden="true" /> Filtres</h2>
-        <h3>Status</h3>
+        <h3>Statuts</h3>
         <ul>
           <li>
             <label>
-              <input type="checkbox" name="ouvert" />
+              <input
+                type="checkbox"
+                name="filterState0"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterState1');
+                  changeField(false, 'filterState2');
+                  changeField(false, 'filterState3');
+                  changeField(false, 'filterState4');
+                }}
+                checked={tournaments.filterState0}
+              />
+              Tous
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                type="checkbox"
+                name="filterState1"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterState0');
+                }}
+                checked={tournaments.filterState1}
+              />
               Ouverts
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="en-cours" />
+              <input
+                type="checkbox"
+                name="filterState3"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterState0');
+                }}
+                checked={tournaments.filterState3}
+              />
               En cours
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="preparation" />
+              <input
+                type="checkbox"
+                name="filterState2"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterState0');
+                }}
+                checked={tournaments.filterState2}
+              />
               En préparation
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="archives" />
+              <input
+                type="checkbox"
+                name="filterState4"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterState0');
+                }}
+                checked={tournaments.filterState4}
+              />
               Archivés
             </label>
           </li>
@@ -110,31 +215,89 @@ function Tournaments() {
         <ul>
           <li>
             <label>
-              <input type="checkbox" name="sh" />
+              <input
+                type="checkbox"
+                name="filterDiscipline0"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline1');
+                  changeField(false, 'filterDiscipline2');
+                  changeField(false, 'filterDiscipline3');
+                  changeField(false, 'filterDiscipline4');
+                  changeField(false, 'filterDiscipline5');
+                }}
+                checked={tournaments.filterDiscipline0}
+              />
+              Tous
+            </label>
+          </li>
+          <li>
+            <label>
+              <input
+                type="checkbox"
+                name="filterDiscipline1"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline0');
+                }}
+                checked={tournaments.filterDiscipline1}
+              />
               Simple Homme
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="sd" />
+              <input
+                type="checkbox"
+                name="filterDiscipline2"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline0');
+                }}
+                checked={tournaments.filterDiscipline2}
+              />
               Simple Dame
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="dh" />
+              <input
+                type="checkbox"
+                name="filterDiscipline3"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline0');
+                }}
+                checked={tournaments.filterDiscipline3}
+              />
               Double Homme
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="dd" />
+              <input
+                type="checkbox"
+                name="filterDiscipline4"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline0');
+                }}
+                checked={tournaments.filterDiscipline4}
+              />
               Double Dame
             </label>
           </li>
           <li>
             <label>
-              <input type="checkbox" name="dx" />
+              <input
+                type="checkbox"
+                name="filterDiscipline5"
+                onChange={(evt) => {
+                  changeField(evt.target.checked, evt.target.name);
+                  changeField(false, 'filterDiscipline0');
+                }}
+                checked={tournaments.filterDiscipline5}
+              />
               Double Mixte
             </label>
           </li>
