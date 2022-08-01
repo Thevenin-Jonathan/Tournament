3469d53e8030386c2fd9709d5af3078596fe338a
Tom R <99020119+Tom-Roche-kinoah@users.noreply.github.com>
2022-08-01 15:54:57 +0200

    feat: create Tournament component

---
+++ front/src/components/Tournament/index.js
@@ -0,0 +1,12 @@
+function Tournament() {
+  return (
+    <main className="content tournament">
+      <h1 className="title">Tournoi xxx</h1>
+
+      <h2>Etape 2/4 </h2>
+
+    </main>
+  );
+}
+
+export default Tournament;
