708d3fe1501c9e05e5b793a88eede08c7c922295
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-01 14:07:23 +0200

    feat: add addUser and removeUser route

--- server/app/routers/v1/teamRouter.js
+++ server/app/routers/v1/teamRouter.js
@@ -3,6 +3,7 @@ const controllerWrapper = require("../../services/controllerWrapper");
 const controller = require("../../controllers/teamController");
 const validationWrapper = require("../../services/validationWrapper");
 const createSchema = require("../../schemas/teamCreate");
+const teamUpdUserSchema = require("../../schemas/teamUpdUser");
 
 router.route("/")
   .get(controllerWrapper(controller.getAll))
@@ -14,5 +15,14 @@ router.route("/:id")
   .get(controllerWrapper(controller.getOne))
   .delete(controllerWrapper(controller.destroy));
 
+router.route("/:id/add-user")
+  .patch(
+    validationWrapper(teamUpdUserSchema),
+    controllerWrapper(controller.addUser));
+
+router.route("/:id/remove-user")
+  .patch(
+    validationWrapper(teamUpdUserSchema),
+    controllerWrapper(controller.removeUser));
 
 module.exports = router;
