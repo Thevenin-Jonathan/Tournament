8a184fdce413eafe91047974e73c4a4e56c575c9
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-01 14:08:24 +0200

    feat: add addUser func

--- server/app/controllers/teamController.js
+++ server/app/controllers/teamController.js
@@ -90,10 +90,54 @@ async function create(req, res) {
   return res.status(201).json(team);
 };
 
-//todo ameliore le get tournament (nb inscrit, id user, id user manager)
-//todo verif nb inscrit
-//todo insertUser
-//todo removeUser
+/**
+ * Add one user into team
+ * 
+ * ExpressMiddleware signature
+ * @param {object} req express request object
+ * @param {object} res express response object
+ * @returns {json} JSON response with the updated team
+ */
+ async function addUser(req, res) {
+  const id = req.params.id;
+  const { user_id } = req.body;
+
+  /** Verify */
+  const team = await teamDatamapper.findById(id);
+  if (!team) {
+    throw new Api404Error("Team does not exist in DB");
+  }
+  
+  const tournament = await tournamentDatamapper.findById(team.tournament_id);
+  if (tournament.state_id >= 2) {
+    throw new ApiError("Unable to add user, tournament already generated");
+  }
+
+  if ((tournament.discipline_id <= 2 && team.users.length === 1) ||
+    (tournament.discipline_id >= 3 && team.users.length === 2)) {
+    throw new ApiError("Unable to add user, team is full");
+  }
+
+  if (tournament.registered.find(user => user.id === user_id)) {
+    throw new ApiError("Unable to add user, already on a team");
+  }
+
+  const user = await userDatamapper.findById(user_id);
+  if (user.gender_id === 1 && (tournament.discipline_id === 2 || tournament.discipline_id === 4)){
+    throw new ApiError("This discipline is reserved for women");
+  } else if (user.gender_id === 2 && (tournament.discipline_id === 1 || tournament.discipline_id === 3)) {
+    throw new ApiError("This discipline is reserved for men");
+  } else if ((tournament.discipline_id === 5 && team.users.length >= 1) && team.users[0].gender_id === user.gender_id) {    
+    throw new ApiError("This discipline is reserved for one men and one women by team");
+  }
+  
+  /** Add user into team */
+  await teamDatamapper.insertUser(id, user_id);
+
+  /** Get and return all team informations */
+  const updTeam = await teamDatamapper.findById(id);
+  return res.json(updTeam)
+}
 
 /**
  * Delete one team from DB
@@ -119,5 +163,6 @@ module.exports = {
   getAll,
   getOne,
   create,
+  addUser,
   destroy
 }
