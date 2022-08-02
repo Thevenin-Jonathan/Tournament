e79a747183a9b4828202a4b9dd321187d806704a
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-02 18:47:08 +0200

    upd: allVsAll schema

--- server/app/schemas/matchCreate.js
+++ server/app/schemas/matchCreate.js
@@ -4,5 +4,6 @@ module.exports = joi.object({
   tournament_id: joi.number().min(1).required(),
   team_ids: joi.array().items(
       joi.number().required()
-  ).length(2).required()
+  ).length(2).required(),
+  phase: joi.number().min(1).required()
 });
