b2f58a6bfa3aa751428409301e48a69b4885b4a7
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-03 16:28:38 +0200

    add err message from error handler to morgan token

--- server/app/services/errorHandler.js
+++ server/app/services/errorHandler.js
@@ -1,4 +1,5 @@
 const debug = require("debug")("error-handler");
+const morgan = require("morgan");
 const ApiError = require("../errors/apiError");
 const Api404Error = require("../errors/api404Error");
 const ApiInternalError = require("../errors/apiInternalError");
@@ -10,18 +11,17 @@ const InvalidTokenError = require("../errors/InvalidTokenError");
  * Error handler middleware.
  * @function errorHandler
  * @param {Error} err - Error object.
- * @param {Object} _ - Express request object. (not used)
+ * @param {Object} req - Express request object.
  * @param {Object} res - Express response object.
  * @param {Function} __ - Express next middleware function. (not used)
  */
-async function errorHandler(err, _, res, __) {
+async function errorHandler(err, req, res, __) {
 
   if (process.env.NODE_ENV === "development") {
     debug(err.statusCode)
     debug(err.message)
     debug(err.stack)
   }
-
 
   if (err instanceof ApiInternalError && process.env.NODE_ENV === "production") {
     err.message = "An error with the server has occured."
@@ -30,6 +30,10 @@ async function errorHandler(err, _, res, __) {
   if (!err.statusCode || isNaN(Number(err.statusCode))) {
     err = new ApiInternalError();
   }
+
+  morgan.token('message', function(_, _) {
+    return err.message;
+  });
 
   res.status(err.statusCode).json({
     code: err.statusCode,
