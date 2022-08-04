d3ab6c06959dee304eb21fdb31aed288b88b8127
Cédric Bernard <bernardcedric@live.fr>
2022-08-04 21:52:16 +0200

    style: Profil et Club

--- front/src/components/Profil/index.js
+++ front/src/components/Profil/index.js
@@ -13,38 +13,50 @@ function Profil() {
       value: user.id,
     });
   }, []);
-
+  console.log(user);
   const userAvatar = `${config.path.uploads.avatar}/${user.avatar}`;
 
   return (
     <main className="content profil">
       <h1 className="title">Mon profil</h1>
 
-      <div className="wrapper">
-        <div className="infos">
-          <h2 className="section-title">{user.firstname} {user.lastname}</h2>
-          <ul>
-            <li><img className="avatar" src={userAvatar} alt={`${user.firstname} Avatar`} /></li>
-            <li>License FFBAD : <span>{user.playerLicense}</span></li>
-            <li>Genre : <span>{genderText(user.genderId)}</span></li>
-            { (user.gender_id === 1)
-              ? <li>Né le : <span>{dateFr(user.birthdate)}</span></li>
-              : <li>Née le : <span>{dateFr(user.birthdate)}</span></li>}
-            <li>Adresse : <span>{user.address}</span></li>
-            <li>Contact : <span>{user.email}</span><span>{formatPhoneNumber(user.phone)}</span></li>
-          </ul>
-        </div>
-        <div className="stats">
-          <h2 className="section-title">Mes statistiques</h2>
-          <ul>
-            <li>Participations totales : <span>3</span></li>
-            <li>Matchs joués : <span>24</span></li>
-            <li>Victoires en simple : <span>1</span></li>
-            <li>Victoires en double : <span>0</span></li>
-            <li>Victoires en mixte : <span>2</span></li>
-          </ul>
-        </div>
+      <div className="wrapper-sections">
+
+        <section className="section-left">
+          <h2 className="member-name">{user.firstname} {user.lastname}</h2>
+          <div className="member-infos">
+            <img
+              className="member-avatar"
+              src={userAvatar}
+              alt={`${user.firstname} Avatar`}
+            />
+          </div>
+
+          <div className="member-infos">license FFBAD : <span>{user.playerLicense}</span></div>
+          <div className="member-infos">Genre : <span>{genderText(user.genderId)}</span></div>
+          { (user.gender_id === 1)
+            ? <div className="member-infos">Née le : <span>{dateFr(user.birthdate)}</span></div>
+            : <div className="member-infos">Né le : <span>{dateFr(user.birthdate)}</span></div>}
+          <div className="member-infos">Adresse : <span>{user.address}</span></div>
+          <div className="member-infos">Contact :
+            <span>{user.email}</span>
+            <span>{formatPhoneNumber(user.phone)}</span>
+          </div>
+
+        </section>
+
+        <section className="section-right">
+          <h2 className="stats-title">Statistiques</h2>
+
+          <div className="member-infos">Participations totales : <span>3</span></div>
+          <div className="member-infos">Matchs joués : <span>24</span></div>
+          <div className="member-infos">Victoires en simple : <span>1</span></div>
+          <div className="member-infos">Victoires en double : <span>0</span></div>
+          <div className="member-infos">Victoires en mixte : <span>2</span></div>
+
+        </section>
       </div>
+
       <Link className="action-btn" to="/profil/modifier-profil">
         <button type="button">
           <i className="fa fa-pencil" aria-hidden="true" />
@@ -52,6 +64,7 @@ function Profil() {
           Modifier mon profil
         </button>
       </Link>
+
     </main>
   );
 }
