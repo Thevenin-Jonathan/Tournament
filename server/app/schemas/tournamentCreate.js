01448b5e998e54148f0d272a62b69475cea43a2e
Thevenin Jonathan <warzonefury@hotmail.fr>
2022-08-05 15:55:30 +0200

    fix: min limit player in joy schema tournament

--- server/app/schemas/tournamentCreate.js
+++ server/app/schemas/tournamentCreate.js
@@ -8,7 +8,7 @@ module.exports = joi.object({
   description: joi.string().min(10),
   picture_url: joi.string(),
   nb_playground: joi.number().required(),
-  player_limit: joi.number(),
+  player_limit: joi.number().min(2),
   discipline_id: joi.number().required(),
   type_id: joi.number().required(),
   state_id: joi.number(),
